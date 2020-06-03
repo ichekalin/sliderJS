1ab24439d9aed176dab1b369c41d4e52ec73ed68
Igor Chekalin <chekalinigor@gmail.com>
2020-06-03 23:52:29 +0300

    changed styles

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let active = 0;
 const img = document.querySelectorAll('img')
 
-document.querySelector('.btn-6').onclick = () => {
+document.querySelector('.btn-slide').onclick = () => {
 	img[active].classList.remove('active')
 	active = ++active % img.length;
 	img[active].classList.add('active')
